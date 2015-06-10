d3b002faeea65233834ca815480fd3aefeddc3ea
Kamal Bin Mustafa <kamal@k4ml.github.io>
2015-06-10 22:42:06 +0800

    initial form

--- web.rb
+++ web.rb
@@ -1,5 +1,18 @@
 require 'sinatra'
 
+require 'teecket'
+require 'teecket/printer'
+
 get '/' do
   "Hello, world"
+  code = "<%= Time.now %>"
+
+  if params[:from] && params[:to] && params[:date]
+    output = Teecket.search({ from: params[:from], to: params[:to], date: params[:date]})
+    @output = Printer.table(output)
+  else
+    @output = ''
+  end
+
+  erb :index
 end
