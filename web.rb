89f214d0b43d289cc9d28fb82be5d90d26e7c45a
Kamal Bin Mustafa <kamal@k4ml.github.io>
2015-06-10 23:27:43 +0800

    put some validation on empty parameters

--- web.rb
+++ web.rb
@@ -4,14 +4,16 @@ require 'teecket'
 require 'teecket/printer'
 
 get '/' do
-  "Hello, world"
-  code = "<%= Time.now %>"
+  @message = ''
+  @output = ''
 
-  if params[:from] && params[:to] && params[:date]
-    output = Teecket.search({ from: params[:from], to: params[:to], date: params[:date]})
-    @output = Printer.table(output)
-  else
-    @output = ''
+  if params[:from] || params[:to] || params[:date]
+    if !params[:from].empty? && !params[:to].empty? && !params[:date].empty?
+      output = Teecket.search({ from: params[:from], to: params[:to], date: params[:date]})
+      @output = Printer.table(output)
+    else
+      @message = "Please enter From, To and Date"
+    end
   end
 
   erb :index
